49b825247caf0cc2c99bff177b789d8381ad7e10
Taha The Developer <tahaalawa366@gmail.com>
2026-07-21 23:43:16 +0300

    style: update text color and background in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="bg-blue-500 text-white p-4 rounded">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="bg-background text-primary p-4 rounded">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
